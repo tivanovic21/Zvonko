e27fdc140a36dbe7acc7c0068b6e07e196a1320a
tivanovic21 <tivanovic21@student.foi.hr>
2024-04-24 19:14:11 +0200

    add sound loading from pc

--- Software/Zvonko/Zvonko/UserControls/UCaddSound.xaml.cs
+++ Software/Zvonko/Zvonko/UserControls/UCaddSound.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -33,6 +34,52 @@ namespace Zvonko.UserControls
                 MainWindow mainWindow = (MainWindow)parentWindow;
                 mainWindow.LoadMainContent();
             }
+        }
+
+        private void btnChooseSound_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            openFileDialog.Filter = "Audio files (*.mp3, *.wav)|*.mp3;*.wav";
+            openFileDialog.FilterIndex = 1;
+
+            bool? result = openFileDialog.ShowDialog();
+
+            if (result == true)
+            {
+                string filePath = openFileDialog.FileName;
+
+                if (IsAudioFile(filePath))
+                {
+                    LoadData(filePath);
+                } else
+                {
+                    MessageBox.Show("Please select an audio file.");
+                }
+            }
+        }
+
+        private bool IsAudioFile(string filePath)
+        {
+            string extension = System.IO.Path.GetExtension(filePath);
+            return extension.Equals(".mp3", StringComparison.OrdinalIgnoreCase) ||
+                   extension.Equals(".wav", StringComparison.OrdinalIgnoreCase);
+        }
+
+        private void LoadData(string filePath)
+        {
+            txtSoundName.Text = System.IO.Path.GetFileName(filePath);
+
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.Open(new Uri(filePath));
+            mediaPlayer.MediaOpened += (sender, e) =>
+            {
+                if (mediaPlayer.NaturalDuration.HasTimeSpan)
+                {
+                    TimeSpan duration = mediaPlayer.NaturalDuration.TimeSpan;
+                    txtSoundLength.Text = duration.ToString(@"hh\:mm\:ss");
+                }
+            };
         }
     }
 }
