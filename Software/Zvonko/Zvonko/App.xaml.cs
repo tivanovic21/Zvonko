0c461fa0214fd99237c48ca9350218591014b615
SimeBraica <sbraica21@foi.hr>
2024-05-21 15:43:12 +0200

    Finished.

--- Software/Zvonko/Zvonko/App.xaml.cs
+++ Software/Zvonko/Zvonko/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BusinessLogicLayer;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Windows;
 
@@ -9,20 +11,52 @@ namespace Zvonko
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
-            //StartPeriodicTask();
+            StartPeriodicTask();
         }
 
         public void StartPeriodicTask() {
             Task.Run(async () => {
                 while (true) {
                     await Task.Delay(600);
-                    // Ensure the MessageBox is shown on the UI thread
                     Application.Current.Dispatcher.Invoke(() => {
-                        MessageBox.Show("Second has passed");
-                        //PlayEvent();
+                        PlayEvent();
                     });
                 }
             });
         }
+
+        private void PlayEvent() {
+            var dayOfTheWeek = DateTime.Now.DayOfWeek;
+            var startingTime = DateTime.Now.TimeOfDay;
+            var date = DateTime.Now.Date;
+
+            EventService eventService = new EventService();
+            RecordingService recordingService = new RecordingService();
+
+            var allEvents = eventService.GetAllEvents();
+            List<String> days = new List<String>();
+
+            foreach (var events in allEvents) {
+                days.Clear();
+                if ((bool)events.monday) days.Add(DayOfWeek.Monday.ToString());
+                if ((bool)events.tuesday) days.Add(DayOfWeek.Tuesday.ToString());
+                if ((bool)events.wednesday) days.Add(DayOfWeek.Wednesday.ToString());
+                if ((bool)events.thursday) days.Add(DayOfWeek.Thursday.ToString());
+                if ((bool)events.friday) days.Add(DayOfWeek.Friday.ToString());
+                if ((bool)events.saturday) days.Add(DayOfWeek.Saturday.ToString());
+                Console.WriteLine("Days for event:");
+                foreach (var day in days) {
+                    if (events.typeOfEventId == 1 && day.Contains(dayOfTheWeek.ToString()) && events.starting_time == startingTime) {
+                        recordingService.PlayRecording(events.Recording);
+                    }
+                }
+               
+
+                if (events.typeOfEventId == 2 && events.date == date && events.starting_time == startingTime) {
+                    recordingService.PlayRecording(events.Recording);
+                }
+            }
+        }
+
     }
 }
