2940a3ee9df29592036c45bd914fe59f8d6ed469
tivanovic21 <tivanovic21@student.foi.hr>
2024-06-07 12:19:00 +0200

    Add specflow project, add initial loginwindow test

---
+++ Software/Zvonko/AcceptanceTests/Features/Login.feature
@@ -0,0 +1,9 @@
+﻿Feature: Login
+
+As a school schedule manager
+I want to be able to login in to the system
+So that I can manage and schedule bell ringing
+
+Scenario: Login From Display
+	When I launch the application
+	Then I should see the login window
---
+++ Software/Zvonko/AcceptanceTests/Support/GuiDriver.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcceptanceTests.Support
+{
+    public static class GuiDriver
+    {
+        private static WindowsDriver<WindowsElement> driver;
+
+        public static WindowsDriver<WindowsElement> GetOrCreateDriver()
+        {
+            if(driver == null)
+            {
+                driver = CreateDriverInstance();
+            }
+            return driver;
+        }
+
+        public static WindowsDriver<WindowsElement> GetDriver()
+        {
+            return driver;
+        }
+
+        private static WindowsDriver<WindowsElement> CreateDriverInstance()
+        {
+            var options = new AppiumOptions();
+
+            string relativePath = Path.Combine("..", "..", "..", "..", "Zvonko", "bin", "Debug", "Zvonko.exe");
+            string exePath = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativePath));
+
+            if (!File.Exists(exePath))
+            {
+                Assert.Fail($"Executable not found: {exePath}");
+            }
+
+            options.AddAdditionalCapability("app", exePath);
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            var wd = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723/"), options);
+            wd.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            return wd;
+        }
+
+        public static void Dispose()
+        {
+            foreach(var wh in driver.WindowHandles)
+            {
+                driver.SwitchTo().Window(wh);
+                driver.CloseApp();
+            }
+            driver.Quit();
+            driver.Dispose();
+            driver = null;
+        }
+    }
+}
