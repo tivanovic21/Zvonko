600581a59074e8bf1bfd50c03ff95a3d4c85d112
Matea Klement <mklement21@foi.hr>
2024-06-13 20:20:13 +0200

    Created unit test for AddRecording method

--- Software/Zvonko/UnitTests/RecordingService_Tests.cs
+++ Software/Zvonko/UnitTests/RecordingService_Tests.cs
@@ -35,5 +35,25 @@ namespace UnitTests {
             Assert.NotNull(result);
             Assert.IsAssignableFrom<IEnumerable<Recording>>(result);
         }
+
+        [Fact]
+        public void AddRecording_DodavanjeZvucnogZapisa_DodajeNoviZapis() {
+            // Arrange
+            var recordingService = new RecordingService();
+            var newRecording = new Recording {
+                name = "Recording Add New Test",
+                duration = TimeSpan.FromMinutes(5),
+                description = "Test description",
+                storedFile = "test_file.mp3",
+                AccountId = 1,
+                timeCreated = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss")
+            };
+
+            // Act
+            var result = recordingService.AddRecording(newRecording);
+
+            // Assert
+            Assert.True(result);
+        }
     }
 }
