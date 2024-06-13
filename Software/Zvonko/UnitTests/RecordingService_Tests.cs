5e57bc284225f50d8fb69acbea644c2d3a38c831
Matea Klement <mklement21@foi.hr>
2024-06-13 19:51:06 +0200

    Removes class and create new class and one test for GetAllRecordings mtehod

---
+++ Software/Zvonko/UnitTests/RecordingService_Tests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using BusinessLogicLayer;
+using DatabaseLayer.Repositories;
+using DatabaseLayer;
+
+namespace UnitTests {
+    public class RecordingService_Tests {
+        [Fact]
+        public void GetAllRecordings_UpravljanjeDogadajima_ReturnsAllRecordings() {
+            // Arrange
+            var recordingService = new RecordingService();
+
+            // Act
+            var result = recordingService.GetAllRecordings();
+
+            // Assert
+            Assert.NotNull(result); 
+            Assert.IsAssignableFrom<IEnumerable<Recording>>(result); 
+        }
+    }
+}
