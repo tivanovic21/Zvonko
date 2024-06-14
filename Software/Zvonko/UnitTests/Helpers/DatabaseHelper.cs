8ab4dd22072442e73c6fdb5339261c4c8feb2572
Matea Klement <mklement21@foi.hr>
2024-06-14 16:49:17 +0200

    Updated DatabaseHelper class to inter new data

--- Software/Zvonko/UnitTests/Helpers/DatabaseHelper.cs
+++ Software/Zvonko/UnitTests/Helpers/DatabaseHelper.cs
@@ -1,14 +1,13 @@
-﻿using DatabaseLayer;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.Configuration;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Data.Common;
+using System.Data.Entity;
+using System.Data.SqlClient;
+using DatabaseLayer;
 
 namespace IntegrationTests.Helpers {
     public static class DatabaseHelper {
-        private static ZvonkoModel9 _dbContext;
+        private static SqlConnection _connection;
 
         public static void ResetDatabase() {
             Connect();
@@ -16,18 +15,59 @@ namespace IntegrationTests.Helpers {
             Disconnect();
         }
 
-        private static void Connect() {
-            _dbContext = new ZvonkoModel9();
-        }
-
         private static void Disconnect() {
-            if (_dbContext != null) {
-                _dbContext.Dispose();
-                _dbContext = null;
+            if (_connection != null && _connection.State != System.Data.ConnectionState.Closed) {
+                _connection.Close();
+                _connection.Dispose();
+                _connection = null;
             }
         }
 
+        private static void Connect() {
+            string connectionString = ConfigurationManager.ConnectionStrings["ZvonkoTest"].ConnectionString;
+            _connection = new SqlConnection(connectionString);
+            _connection.Open();
+        }
+
+
         private static void ResetTables() {
+            ClearTables();
+            InsertDefaultData();
+        }
+
+        private static void ClearTables() {
+            string sqlScript = @"
+            DELETE FROM [dbo].[Events];
+            DELETE FROM [dbo].[Recordings];
+            DELETE FROM [dbo].[Accounts];
+            DELETE FROM [dbo].[TypeOfEvent];";
+            ExecuteSqlCommand(sqlScript);
+        }
+
+        private static void InsertDefaultData() {
+            string sqlScript = @"
+            INSERT INTO [dbo].[Accounts] (username, password, schoolName, macAddress) VALUES
+            ('TestUser1', 'password1', 'TestSchool1', '00:00:00:00:00:01'),
+            ('TestUser2', 'password2', 'TestSchool2', '00:00:00:00:00:02');
+
+            INSERT INTO [dbo].[TypeOfEvent] (typeName, isRecurring) VALUES
+            ('EventType1', 1),
+            ('EventType2', 0);
+
+            INSERT INTO [dbo].[Recordings] (name, duration, description, storedFile, AccountId, timeCreated) VALUES
+            ('Test Recording 1', '00:05:00', 'Test Description 1', 'test_file1.mp3', 1, GETDATE()),
+            ('Test Recording 2', '00:10:00', 'Test Description 2', 'test_file2.mp3', 2, GETDATE());
+
+            INSERT INTO [dbo].[Events] (name, description, starting_time, accountId, recordingId, typeOfEventId, date, monday, tuesday, wednesday, thursday, friday, saturday, sunday) VALUES
+            ('Test Event 1', 'Description 1', '08:00:00', 1, 1, 1, '2023-06-14', 1, 0, 0, 0, 0, 0, 0),
+            ('Test Event 2', 'Description 2', '09:00:00', 2, 2, 2, '2023-06-15', 0, 1, 0, 0, 0, 0, 0);";
+            ExecuteSqlCommand(sqlScript);
+        }
+
+        private static void ExecuteSqlCommand(string sqlCommand) {
+            using (SqlCommand command = new SqlCommand(sqlCommand, _connection)) {
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
