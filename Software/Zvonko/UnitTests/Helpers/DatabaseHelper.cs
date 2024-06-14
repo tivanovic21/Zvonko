290419c9daa143282cbb054918692be3db4bda71
Matea Klement <mklement21@foi.hr>
2024-06-14 16:14:12 +0200

    Renamed UnitTests into IntegrationTests and created initial DatabaseHelper class.

---
+++ Software/Zvonko/UnitTests/Helpers/DatabaseHelper.cs
@@ -0,0 +1,33 @@
+﻿using DatabaseLayer;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntegrationTests.Helpers {
+    public static class DatabaseHelper {
+        private static ZvonkoModel9 _dbContext;
+
+        public static void ResetDatabase() {
+            Connect();
+            ResetTables();
+            Disconnect();
+        }
+
+        private static void Connect() {
+            _dbContext = new ZvonkoModel9();
+        }
+
+        private static void Disconnect() {
+            if (_dbContext != null) {
+                _dbContext.Dispose();
+                _dbContext = null;
+            }
+        }
+
+        private static void ResetTables() {
+        }
+    }
+}
